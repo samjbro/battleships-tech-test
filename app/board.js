004239e4aeb216f240b17823bbb070e46da9a8e3
samjbro <sjbroughton1@gmail.com>
2017-02-28 18:14:46 +0000

    ships cannot be placed at occupied locations

--- app/board.js
+++ app/board.js
@@ -7,7 +7,17 @@ Board.prototype = {
     return this._fleet;
   },
   place: function(ship){
+    if(this._getOccupiedLocations().includes(ship.location)){
+      throw new Error('Location already occupied!');
+    }
     this._fleet.push(ship);
+  },
+  _getOccupiedLocations: function(){
+    var occupiedLocations = []
+    for(var ship = 0; ship < this._fleet.length; ship++){
+      occupiedLocations.push(this._fleet[ship].location);
+    }
+    return occupiedLocations;
   }
 };
 
--- test/test.board.js
+++ test/test.board.js
@@ -1,15 +1,22 @@
 describe('Board', function(){
-  var board;
-
+  var board,
+      ship;
   beforeEach(function(){
     board = new Board();
+    ship = sinon.createStubInstance(Ship);
   });
   it('has no ships initially', function(){
     expect(board.ships()).to.eql([]);
   });
   it('can place a ship', function(){
-    var ship = sinon.createStubInstance(Ship);
     board.place(ship);
     expect(board.ships()).to.eql([ship]);
+  });
+  it('prevents a ship being placed at an occupied location', function(){
+    ship.location = 'C4';
+    board.place(ship);
+    var otherShip = sinon.createStubInstance(Ship);
+    otherShip.location = 'C4';
+    expect(function(){ board.place(otherShip); }).to.throw('Location already occupied!');
   });
 });
